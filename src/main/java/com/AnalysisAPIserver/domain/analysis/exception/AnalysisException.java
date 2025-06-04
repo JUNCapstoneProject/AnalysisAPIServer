2504f919c5e746274918f2538c2cb7af801d85d7
JNU\203075 <hellcat27@hanmail.net>
2025-06-04 18:37:57 +0900

    pom 수정

--- src/main/java/com/AnalysisAPIserver/domain/analysis/exception/AnalysisException.java
+++ src/main/java/com/AnalysisAPIserver/domain/analysis/exception/AnalysisException.java
@@ -1,16 +1,11 @@
 package com.AnalysisAPIserver.domain.analysis.exception;
 
-/**
- * 분석 관련 예외 클래스이다.
- */
-public final class AnalysisException extends RuntimeException {
-
-    /**
-     * AnalysisException 생성자.
-     *
-     * @param message 에러 메시지
-     */
-    public AnalysisException(final String message) {
+public class AnalysisException extends RuntimeException {
+    public AnalysisException(String message) {
         super(message);
+    }
+
+    public AnalysisException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
